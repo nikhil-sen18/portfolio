2abdcf7a690d0e45b6c7b905eaf6d6cf928f54f8
nikhil-sen18 <sennikhil221@gmail.com>
2024-06-23 02:49:33 +0530

    Navbar

--- components/main/navbar.tsx
+++ components/main/navbar.tsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 px-10">
+    <div className="w-full h-[65px] fixed top-0 shadow-lg shadow-[#2A0E61]/50 bg-[#03001417] backdrop-blur-md z-50 md:px-10 ">
       <div className="w-full h-full flex flex-row items-center justify-between m-auto px-[10px] ">
         <Link
           href="/"
@@ -25,15 +25,15 @@ const Navbar = () => {
           </span>
         </Link>
 
-        <div className="w-[500px] h-full flex flex-row items-center justify-between md:mr-20">
-          <div className="flex items-center justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
-            <Link href="/" className="cursor-pointer" >
+        <div className="w-[500px] h-full sm:justify-end flex flex-row  items-center  justify-between md:mr-20 ">
+          <div className="flex items-center  justify-between w-full h-auto border border-[#7042f861] bg-[#0300145e] mr-[15px] px-[20px] py-[10px] rounded-full text-gray-200">
+            <Link href="/" className="cursor-pointer text-sm md:text-base" >
               About me
             </Link>
-            <Link href="/skills" className="cursor-pointer">
+            <Link href="/skills" className="cursor-pointer text-sm md:text-base">
               Skills
             </Link>
-            <Link href="#/rojects" className="cursor-pointer">
+            <Link href="#/rojects" className="cursor-pointer text-sm md:text-base">
               Projects
             </Link>
           </div>
